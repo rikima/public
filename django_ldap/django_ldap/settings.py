ee04da8ebbdafa7db463c669b05922ee0bf1ffff
Maeda Mikio <maeda_mikio@wakamurasaki.com>
2018-05-07 10:52:47 +0900

    デバック用に接続元ホスト、日本語環境を編集

--- django_ldap/django_ldap/settings.py
+++ django_ldap/django_ldap/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '1n7x#*35n$6vb2$%6#619q+&d=+ca&ed!63q4we*13vb8sq#va'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -103,9 +103,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ja'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Tokyo'
 
 USE_I18N = True
 
