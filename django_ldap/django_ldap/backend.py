16c06e04ef71229972125801a2f558e3ee656a4c
前田 幹夫 <mikio.maeda@brainpad.co.jp>
2018-05-09 09:04:29 +0900

    pylintで修正

--- django_ldap/django_ldap/backend.py
+++ django_ldap/django_ldap/backend.py
@@ -1,3 +1,10 @@
+"""
+Active Directory対応の認証バックエンド
+Active DirectoryのユーザーIDとパスワードで認証を行う
+認証以外の認可等は、対応しない
+参考URL https://docs.djangoproject.com/en/2.0/topics/auth/customizing/
+"""
+
 from ssl import CERT_NONE, PROTOCOL_SSLv23
 from ldap3 import Server, Connection, Tls
 
@@ -6,17 +13,34 @@ from django.contrib.auth.models import User
 
 
 class Backend:
+    """
+    Active Directory用の認証バックエンドクラス
 
+    settings.pyに以下を設定することで、本バックエンドを変更している
+    AUTHENTICATION_BACKENDS = ['django_ldap.backend.Backend']
+    LDAP_HOST = 'your-ldap-host-ip-address'
+    LDAP_PORT = 636
+    LDAP_DOMAIN = 'your-ldap-domain'
+    """
 
     def authenticate(self, request, username=None, password=None):
+        """
+        独自バックエンドに必須のメソッド
+
+        Active Directoryへusernameとpasswordでbindして認証を実施
+        該usernameがDjangoに存在しない場合は、新規作成する必要がある
+        """
 
         if not (username and password):
             return None
 
         tls = Tls(validate=CERT_NONE, version=PROTOCOL_SSLv23)
         server = Server(host=settings.LDAP_HOST, port=settings.LDAP_PORT, use_ssl=True, tls=tls)
-        conn = Connection(server, user='{}\\{}'.format(settings.LDAP_DOMAIN, username), password=password)
-        if not conn.bind():
+        connection = Connection(
+            server,
+            user='{}\\{}'.format(settings.LDAP_DOMAIN, username),
+            password=password)
+        if not connection.bind():
             return None
 
         try:
@@ -30,6 +54,11 @@ class Backend:
 
 
     def get_user(self, user_id):
+        """
+        独自バックエンドに必須のメソッド
+
+        参考URLと同じ
+        """
         try:
             return User.objects.get(pk=user_id)
         except User.DoesNotExist:
