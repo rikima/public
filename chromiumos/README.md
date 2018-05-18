baa7e07281c781043b1b928381bbd0e2242604d2
Maeda Mikio <maeda_mikio@wakamurasaki.com>
2018-05-18 14:15:45 +0900

    move chromiumos

---
+++ chromiumos/README.md
@@ -0,0 +1,20 @@
+# Chromiumos Image Build Shell
+
+# Document
+http://dev.chromium.org/chromium-os/quick-start-guide
+
+# Environment
+- CPU: 3GHz
+- MEM: 8G
+- HDD: 100G
+- OS: Ubuntu 14.04
+
+# Run Build
+```
+$ ./build.sh
+```
+
+# Check Image
+```
+$ ls chroot/home/ubuntu/chromiumos.iso
+```
---
+++ chromiumos/build.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+sudo apt-get install -y git-core gitk git-gui subversion curl lvm2 thin-provisioning-tools python-pkg-resources python-virtualenv expect
+git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git
+export PATH=$PATH:$PWD/depot_tools
+git config --global user.email "you@example.com"
+git config --global user.name "Your Name"
+(
+echo '#!/bin/sh';
+echo 'echo Defaults \!tty_tickets > $1';
+echo 'echo Defaults timestamp_timeout=180 >> $1';
+) > sudo_editor
+chmod +x ./sudo_editor
+sudo EDITOR=./sudo_editor visudo -f /etc/sudoers.d/relax_requirements
+expect -c "
+set timeout -1
+spawn repo init -u https://chromium.googlesource.com/chromiumos/manifest.git
+expect \"Enable color display in this user account (y/N)?\"
+send \"y\n\"
+"
+repo sync
+export BOARD=amd64-generic
+cros_sdk -- ./build_packages --board=${BOARD}
+cros_sdk -- ./build_image --board=${BOARD}
+cros_sdk -- cros flash --board=${BOARD} file:///home/ubuntu/chromiumos.iso
